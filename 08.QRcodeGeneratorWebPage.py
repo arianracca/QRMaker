e18cb023b1909ee2ed9bc5ca5bb716b7bc0d020f
Arian Racca <arianracca@gmail.com>
2022-08-31 22:43:18 -0300

    Save to folder, select name file. OK

--- 08.QRcodeGeneratorWebPage.py
+++ 08.QRcodeGeneratorWebPage.py
@@ -1,6 +1,18 @@
+# QRcodeWebPages.py - A simple but flexible QR code maker
+# Used pyqrcode, png, os
 import pyqrcode
 import png
-url = input("Hola, ingrese la web de la que desea generar un código QR de ingreso en formato https://nombre.xx ")
+import os
 
-qr_code = pyqrcode.create(url)
-qr_code.png("QRURL.png",scale=5)
+http = "https://www."
+url_name = input("""Hola, ingrese la web de la que desea
+generar un código QR.
+formato: nombre.xx (sin https://www.
+""")
+url_link = http+url_name
+url_title = input("Elija el nombre del archivo a guardar\n")
+folder = input("Ingrese la ruta de acceso a la carpeta de guardado\n")
+qr_img = pyqrcode.create(url_link)
+#qr_img.png("./"+url_name+".png",scale=10)
+qr_img.png(os.path.join(folder, url_title+".png"),scale=10)
+print("Se generó el código QR de la web "+url_name)
